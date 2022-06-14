1c44bb303307f285c9483af3d068c25dca8f2a0d
Esteban ANTONIO-MOTA <94258469+estebanantonio@users.noreply.github.com>
2022-06-14 18:16:56 -0400

    DEBUG

--- sinergis/models/company.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    x_sinergis_res_company_iban = fields.Char(string="IBAN")
-    x_sinergis_res_company_bic = fields.Char(string="BIC")
-    
