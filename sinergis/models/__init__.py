8e2ef23c57ea5e782cb2f31a7712cf895d2a2d3f
Esteban ANTONIO-MOTA <94258469+estebanantonio@users.noreply.github.com>
2022-06-14 18:21:24 -0400

    OK

---
+++ sinergis/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import partner
+from . import helpdesk
+from . import calendar
+from . import sale
+from . import project
+from . import users
+from . import company
+from . import account
---
+++ sinergis/models/users.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    x_sinergis_res_users_tampon_signature = fields.Html(string="Tampon Signature")
+    
