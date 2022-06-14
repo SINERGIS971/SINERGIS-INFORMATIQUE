623fbdfe941f230eaace3bd076173eace7ffc6a0
Esteban ANTONIO-MOTA <94258469+estebanantonio@users.noreply.github.com>
2022-06-14 18:13:05 -0400

    DEBUG

--- sinergis/models/account.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
-
-
-class AccountAnalyticLine(models.Model):
-    _inherit = "account.analytic.line"
-    x_sinergis_account_analytic_line_user_id = fields.Many2one('res.users', string="Employé")
--- sinergis/models/users.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-from odoo.exceptions import ValidationError
-
-
-class ResUsers(models.Model):
-    _inherit = "res.users"
-
-    x_sinergis_res_users_tampon_signature = fields.Html(string="Tampon Signature")
-    
