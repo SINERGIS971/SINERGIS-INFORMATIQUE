829a60825558dec5f1d66b51b01e1514f47f2f74
Esteban ANTONIO-MOTA <94258469+estebanantonio@users.noreply.github.com>
2022-06-20 16:04:58 -0400

    Update mail.py

--- sinergis/models/mail.py
+++ sinergis/models/mail.py
@@ -22,5 +22,18 @@ class MailActivity(models.Model):
                 'default_x_sinergis_calendar_event_contact_transfered': self.env['helpdesk.ticket'].search([('id','=',res_id)]).x_sinergis_helpdesk_ticket_contact.id,
             }
             return action
+        if res_model == "project.task":
+            self.ensure_one()
+            action = self.env["ir.actions.actions"]._for_xml_id("calendar.action_calendar_event")
+            res_id = self.env.context.get('default_res_id')
+            action['context'] = {
+                'default_activity_type_id': self.activity_type_id.id,
+                'default_res_id': res_id,
+                'default_res_model': res_model,
+                'default_name': self.env['project.task'].search([('id','=',res_id)]).name,
+                'default_description': self.note if not is_html_empty(self.note) else '',
+                'default_activity_ids': [(6, 0, self.ids)],
+                'default_x_sinergis_calendar_event_client': self.env['project.task'].search([('id','=',res_id)]).partner_id.id,
+                'default_x_sinergis_calendar_event_facturation': 'Devis',
+            }
         return
-  
