82084bfe1aea6ee8797edf790689677a3818dfb7
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-21 09:14:51 -0400

    Mise a jour des signatures

--- sinergis/models/users.py
+++ sinergis/models/users.py
@@ -5,5 +5,4 @@ from odoo.exceptions import ValidationError
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    x_sinergis_res_users_tampon_signature = fields.Html(string="Tampon Signature")
-    
+    x_sinergis_res_users_tampon_signature = fields.Image(string="Signature document")
