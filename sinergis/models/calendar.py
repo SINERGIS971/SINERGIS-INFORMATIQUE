ce29c71ca020fa9728742cc7f8493b7c4f060516
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-23 09:36:44 -0400

    Update calendar.py

--- sinergis/models/calendar.py
+++ sinergis/models/calendar.py
@@ -62,6 +62,7 @@ class CalendarEvent(models.Model):
 
     x_sinergis_calendar_event_is_solved = fields.Boolean(string="L'intervention est-elle terminée ?'",default=False)
 
+
     x_sinergis_calendar_event_intervention_count = fields.Integer(string="Nombre d'interventions", compute="_compute_x_sinergis_calendar_event_intervention_count")
     x_sinergis_calendar_event_temps_cumule = fields.Float(string="Temps cumulé", compute="_compute_x_sinergis_calendar_event_temps_cumule", group_operator=True)
 
