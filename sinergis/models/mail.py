4d97cfbb9ce311e1affca23aadcfbea2ce97fe05
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-23 09:00:21 -0400

    Update mail.py

--- sinergis/models/mail.py
+++ sinergis/models/mail.py
@@ -50,7 +50,7 @@ class MailActivity(models.Model):
                 'default_name': self.env['project.task'].search([('id','=',res_id)]).name,
                 'default_description': self.note if not is_html_empty(self.note) else '',
                 'default_activity_ids': [(6, 0, self.ids)],
-                'default_x_sinergis_calendar_event_client': self.env['res.partner'].search([('id','=',res_id)]).id,
+                'default_x_sinergis_calendar_event_client': self.env['res.partner'].search([('id','=',res_id)]).id
             }
             return action
         else:
