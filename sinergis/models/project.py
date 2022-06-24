67380c6e98960c519ac219b907363d56e10d9402
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-24 11:35:43 -0400

    Update project.py

--- sinergis/models/project.py
+++ sinergis/models/project.py
@@ -47,8 +47,8 @@ class ProjectTask(models.Model):
         if tache.effective_hours>=tache.planned_hours:
             self.x_sinergis_project_task_alerte = "Attention ! Le contrat est terminé, merci de consulter un commercial."
         elif tache.effective_hours>=0.9*tache.planned_hours:
-            hours = int(tache.remaining_hours)
-            minutes = int((tache.remaining_hours - hours)*60)
+            hours = ceil(tache.remaining_hours)
+            minutes = ceil((tache.remaining_hours - hours)*60)
             self.x_sinergis_project_task_alerte = "Attention ! Il reste uniquement " + str(hours) + " heures et " + str(minutes) + " minutes sur le contrat"
         else : self.x_sinergis_project_task_alerte = False
 
