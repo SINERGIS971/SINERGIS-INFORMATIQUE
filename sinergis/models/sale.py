312caca42cc2d3caeb63905f7610c6c5b00f5a8e
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-27 17:20:29 -0400

    Update sale.py

--- sinergis/models/sale.py
+++ sinergis/models/sale.py
@@ -12,7 +12,7 @@ class SaleOrder(models.Model):
     x_sinergis_sale_order_objet = fields.Char(string="Objet")
     x_sinergis_sale_order_contact = fields.Many2one("res.partner",string="Contact", required="True")
 
-    fiscal_position_id = fields.Many2one(compute="_compute_fiscal_position_id", readonly=False, domain="[('company_id','=',company_id)]");
+    fiscal_position_id = fields.Many2one(readonly=False, domain="[('company_id','=',company_id)]");
 
     #Empeche l'actualisation automatique de la position fiscale en fonction de la société, nous la recalculons directement en compute en fonction du pays de provenance du client
     """@api.onchange('partner_shipping_id', 'partner_id', 'company_id')
