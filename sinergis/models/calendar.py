29bea779c91355d1639513c078fae96e87422a54
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-27 18:10:27 -0400

    Update calendar.py

--- sinergis/models/calendar.py
+++ sinergis/models/calendar.py
@@ -127,7 +127,7 @@ class CalendarEvent(models.Model):
         self.x_sinergis_calendar_duree_facturee = self.duration
         if self.x_sinergis_calendar_event_start_time == False and self.x_sinergis_calendar_event_end_time == False :
             self.x_sinergis_calendar_event_start_time = self.start
-            self.x_sinergis_calendar_event_end_time = self.end
+            self.x_sinergis_calendar_event_end_time = self.stop
 
     @api.onchange("x_sinergis_calendar_event_produits")
     def on_change_x_sinergis_calendar_event_produits(self):
