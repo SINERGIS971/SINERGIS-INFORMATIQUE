089666c4cbf62ea20badd932b8e1179920b24254
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-27 12:06:01 -0400

    Update account.py

--- sinergis/models/account.py
+++ sinergis/models/account.py
@@ -4,8 +4,8 @@ from odoo.exceptions import ValidationError
 
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
-    x_sinergis_account_analytic_line_start_time = fields.Many2one('res.users', string="Début")
-    x_sinergis_account_analytic_line_end_time = fields.Many2one('res.users', string="Fin")
+    x_sinergis_account_analytic_line_start_time = fields.Datetime(string="Début")
+    x_sinergis_account_analytic_line_end_time = fields.Datetime(string="Fin")
     x_sinergis_account_analytic_line_user_id = fields.Many2one('res.users', string="Employé")
     x_sinergis_account_analytic_line_ticket_id = fields.Many2one('helpdesk.ticket', string="Ticket")
     x_sinergis_account_analytic_line_event_id = fields.Many2one('calendar.event', string="Evenement")
