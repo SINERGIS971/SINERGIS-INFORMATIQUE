f022a4f7f796b0ace2eaa5bb870a1d7b69e47dda
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-29 08:58:22 -0400

    Update sale.py

--- sinergis/models/sale.py
+++ sinergis/models/sale.py
@@ -90,4 +90,4 @@ class SaleOrder(models.Model):
 
     #METTRE LES CONDITIONS DE PAIEMENT PAR DEFAUT - OVERRIDE FONCTION DE BASE
     payment_term_id = fields.Many2one(
-        'account.payment.term', string='Payment Terms', check_company=True,domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",default=lambda self: self.env['account.payment.term'].search([('name','=',"100% des logiciels et des contrats d'heures et 50% des prestations et formations. Le solde à livraison.")]))
+        'account.payment.term', string='Payment Terms', check_company=True,domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]",default=lambda self: self.env['account.payment.term'].search([('name','ilike',"100% des logiciels")]))
