ec2cfedaab8f9ed2653ab0cda6b7e82fe1ae8784
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-30 11:23:55 -0400

    maj

--- sinergis/models/movementArea.py
+++ sinergis/models/movementArea.py
@@ -3,10 +3,10 @@ from odoo import models, fields, api
 class SinergisMovementCountry(models.Model):
     _name = "sinergis.movementcountry"
     _description = "Pays de déplacement"
-    x_sinergis_movement_country_name = fields.Char(string="Nom du pays")
+    name = fields.Char(string="Nom du pays")
 
 class SinergisMovementArea(models.Model):
     _name = "sinergis.movementarea"
     _description = "Zone de déplacement du pays"
-    x_sinergis_movement_area_name = fields.Char(string="Nom de la zone")
+    name = fields.Char(string="Nom de la zone")
     x_sinergis_movement_area_country = fields.Many2one("sinergis.movementcountry",string="Pays")
--- sinergis/models/partner.py
+++ sinergis/models/partner.py
@@ -152,6 +152,8 @@ class ResPartner(models.Model):
     x_sinergis_societe_contact_facturation_zip = fields.Char(string='Code postal')
     x_sinergis_societe_contact_facturation_country_id = fields.Many2one('res.country',string='Pays')
 
+    x_sinergis_societe_contact_inactive = fields.Boolean(string="Contact inactif")
+
 
     @api.onchange("x_sinergis_societe_contact_firstname")
     def on_change_x_sinergis_societe_contact_firstname(self):
