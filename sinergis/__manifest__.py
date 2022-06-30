33bfebb2d2d274ba9b428a1180ac28a969ee1b1e
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-06-30 10:53:01 -0400

    MAJ

--- sinergis/__manifest__.py
+++ sinergis/__manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/partner.xml',
         'views/helpdesk.xml',
         'views/calendar.xml',
