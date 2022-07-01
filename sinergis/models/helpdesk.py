5221c227c636f9b4bda5fbcff6ec839bc77a68b8
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-07-01 17:48:28 -0400

    MAJ

--- sinergis/models/helpdesk.py
+++ sinergis/models/helpdesk.py
@@ -287,6 +287,7 @@ class HelpdeskTicket(models.Model):
 
     def x_sinergis_helpdesk_ticket_show_facturation_button (self):
         self.x_sinergis_helpdesk_ticket_show_facturation = not self.x_sinergis_helpdesk_ticket_show_facturation
+        self.x_sinergis_helpdesk_ticket_temps_passe = (self.x_sinergis_helpdesk_ticket_end_time - self.x_sinergis_helpdesk_ticket_start_time).total_seconds() / 3600
 
     def x_sinergis_helpdesk_ticket_duree_button(self):
         if self.x_sinergis_helpdesk_ticket_temps_passe <= 0 and self.x_sinergis_helpdesk_ticket_is_facturee == False:
