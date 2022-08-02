f24f7fefefaa8dfaa5de2dca5872388438f38d15
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-08-02 13:06:16 -0400

    MAJ

--- sinergis/__manifest__.py
+++ sinergis/__manifest__.py
@@ -55,9 +55,10 @@
         'web.assets_backend': [
             'sinergis/static/src/js/button_confirm.js',
         ],
-        #'web.assets_qweb': [
-        #    'sinergis/static/src/xml/menu_company.xml',
-        #],
+        'web.assets_qweb': [
+            #'sinergis/static/src/xml/menu_company.xml',
+            'sinergis/static/src/xml/help_button.xml',
+        ],
     },
 
 }
