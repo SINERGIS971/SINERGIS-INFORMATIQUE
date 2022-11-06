27070c92aecdb430bb5c4c4db84d207aa0a8980b
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2022-11-06 16:10:15 +0100

    MAJ

--- sinergis/models/account.py
+++ sinergis/models/account.py
@@ -7,5 +7,5 @@ class AccountAnalyticLine(models.Model):
     x_sinergis_account_analytic_line_start_time = fields.Datetime(string="Début")
     x_sinergis_account_analytic_line_end_time = fields.Datetime(string="Fin")
     x_sinergis_account_analytic_line_user_id = fields.Many2one('res.users', string="Employé")
-    x_sinergis_account_analytic_line_ticket_id = fields.Many2one('helpdesk.ticket', string="Ticket")
-    x_sinergis_account_analytic_line_event_id = fields.Many2one('calendar.event', string="Evenement")
+    x_sinergis_account_analytic_line_ticket_id = fields.Many2one('helpdesk.ticket', string="Ticket", readonly=True)
+    x_sinergis_account_analytic_line_event_id = fields.Many2one('calendar.event', string="Evenement", readonly=True)
