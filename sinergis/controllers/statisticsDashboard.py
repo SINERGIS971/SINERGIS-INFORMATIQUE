021f59a15ced7d1078a6db1e07205b604fe71545
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-16 12:39:41 +0100

    Update statisticsDashboard.py

--- sinergis/controllers/statisticsDashboard.py
+++ sinergis/controllers/statisticsDashboard.py
@@ -6,11 +6,22 @@ class StatisticsDashboard(http.Controller):
     @http.route('/sinergis/statistics_dashboard', auth='user', methods=['GET','POST'])
     def index (self, **kw):
         uid = request.uid
+
+        #DEBUG
         #Add user group verification
         today = date.today()
         data = ""
-        tasks = request.env["project.task"].search(['&amp;',('project_id.name','ilike','HEURES'),('project_id.name','ilike','CONTRAT D')])
+        #Filtre pour tâches CH non archivées et archivées
+        n = 0
+        #END DEBUG
+
+        total_time = 0
+
+        tasks = request.env["project.task"].search(['|','&','&',('active','=',False),('active','=',True),('create_date','<=','2023-02-16'),'&',('project_id.name','ilike','HEURES'),('project_id.name','ilike','CONTRAT D')])
         for task in tasks :
+            planned_hours = task.planned_hours
+            timesheet_ids = task.timesheet_ids
             data += task.name
             data += "<br/>"
-        return data
+            n+=1
+        data+=str(n)
