d81235a4ecda06ae8561c939d73ed063597c52a2
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-16 12:09:26 +0100

    laj

--- sinergis/controllers/statisticsDashboard.py
+++ sinergis/controllers/statisticsDashboard.py
@@ -1,7 +1,16 @@
 from odoo import http
 from odoo.http import request
+from datetime import date
 
 class StatisticsDashboard(http.Controller):
     @http.route('/sinergis/statistics_dashboard', auth='user', methods=['GET','POST'])
     def index (self, **kw):
-        return request.session.uid
+        uid = request.uid
+        #Add user group verification
+        today = date.today()
+        data = ""
+        tasks = request.env["project.task"].search(['&amp;',('project_id.name','ilike','HEURES'),('project_id.name','ilike','CONTRAT D')])
+        for task in tasks :
+            data += task.name
+            data += "<br/>"
+        return data
