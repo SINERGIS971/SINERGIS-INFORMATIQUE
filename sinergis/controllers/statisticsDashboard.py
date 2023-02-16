f0e572834b1f68f3de3308758ac67be0e1a0a82c
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-16 14:47:19 +0100

    maj

--- sinergis/controllers/statisticsDashboard.py
+++ sinergis/controllers/statisticsDashboard.py
@@ -1,34 +1,33 @@
 from odoo import http
 from odoo.http import request
 from datetime import date
+from datetime import datetime
 
 class StatisticsDashboard(http.Controller):
-    @http.route('/sinergis/statistics_dashboard', auth='user', methods=['GET','POST'])
+    @http.route('/sinergis/statistics_dashboard', auth='user', methods=['GET','POST'], csrf=False)
     def index (self, **kw):
         uid = request.uid
-
-        #DEBUG
         #Add user group verification
-        today = date.today()
-        data = ""
+        date_end = date.today()
+        if "hourly_contract_date" in kw :
+            date_end = datetime.strptime(kw["hourly_contract_date"], '%Y-%m-%d').date()
         #Filtre pour tâches CH non archivées et archivées
-        n = 0
-        #END DEBUG
 
         total_hours = 0
 
-        tasks = request.env["project.task"].search(['|','&','&',('active','=',False),('active','=',True),('create_date','<=','2023-02-16'),'&',('project_id.name','ilike','HEURES'),('project_id.name','ilike','CONTRAT D')])
+        tasks = request.env["project.task"].search(['|','&','&',('active','=',False),('active','=',True),('create_date','<=',date_end.strftime("%Y-%m-%d")),'&',('project_id.name','ilike','HEURES'),('project_id.name','ilike','CONTRAT D')])
         for task in tasks :
             #Par on fixe le nombre d'heures au temps prévu par le CH
             task_hours = task.planned_hours
             # On soustrait ensuite chaque intervention avant la date demandée
-            timesheet_ids = request.env["account.analytic.line"].search([('task_id', '=', task.id)])
+            timesheet_ids = request.env["account.analytic.line"].search([('task_id', '=', task.id),('create_date','<=',date_end.strftime("%Y-%m-%d"))])
             for timesheet_id in timesheet_ids :
                 task_hours -= timesheet_id.unit_amount
             #Mettre le CH à 0 si il a un nombre d'heures restantes négative
             if task_hours < 0 :
                 task_hours = 0
             total_hours += task_hours
+            total_hours = round(total_hours, 2)
         return http.request.render("sinergis.statistics_dashboard_page", {
-                            "total_hours" : total_hours,
+                            "total_hours" : str(kw),
                         })
