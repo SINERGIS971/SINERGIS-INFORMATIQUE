2dea811f8b9030b42dc68cd979ebcdb657d9ee9f
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-17 11:43:29 +0100

    MàJ dashboard

--- sinergis/controllers/statisticsDashboard.py
+++ sinergis/controllers/statisticsDashboard.py
@@ -56,32 +56,140 @@ class StatisticsDashboard(http.Controller):
 class InvoiceExcelReportController(http.Controller):
     @http.route(['/excel'], type='http', auth="user", csrf=False)
     def get_sale_excel_report(self, report_id=None, **args):
+        #Paramètres de génération :
+        begin_date = date.today()
+        end_date = date.today()
+        #Chargement des données
+        # En sortie de la fonction nous obtenons deux tableaux. Le premier pour les CH consommés à end_date
+        # et le second pour les CH non consommés
+        data_consumed, data_not_consumed = InvoiceExcelReportController.get_hour_contract_data(request, begin_date, end_date)
+        
+        # Création de la réponse Http
         response = request.make_response(
         None,
         headers=[
            ('Content-Type', 'application/vnd.ms-excel'),
-            #('Content-Disposition', 'Invoice_report' + '.xlsx')
            ("Content-disposition", "attachment;filename=myExcel.xls")
         ]
         )
         output = io.BytesIO()
         workbook = xlsxwriter.Workbook(output, {'in_memory': True})
         header_format = workbook.add_format({'bold': True, 'font_color': 'blue'})
-        # prepare excel sheet styles and formats
-        sheet = workbook.add_worksheet("CH NON CONSOMMES A 100%")
-        sheet.set_column(0, 7, 30)
+        red_text = workbook.add_format({'font_color': 'red'})
+        green_text = workbook.add_format({'font_color': 'green'})
 
-        sheet.write(0, 0, 'Date de création du contrat', header_format)
-        sheet.write(0, 1, "Date d'expiration", header_format)
-        sheet.write(0, 2, 'Bon de commande N°', header_format)
-        sheet.write(0, 3, 'Client', header_format)
-        sheet.write(0, 4, 'Heures prévues initialement', header_format)
-        sheet.write(0, 5, 'Heures passés entre le 01/01/2022 et 31/12/2022', header_format)
-        sheet.write(0, 6, 'Heures restantes le 31/12/2022', header_format)
-        sheet.write(0, 7, 'Société', header_format)
-       
+        #CH NON CONSOMMES
+        sheet_1 = workbook.add_worksheet("CH NON CONSOMMES A 100%")
+        sheet_1.set_column(0, 7, 30)
+
+        sheet_1.write(0, 0, 'Date de création du contrat', header_format)
+        sheet_1.write(0, 1, "Date d'expiration", header_format)
+        sheet_1.write(0, 2, 'Bon de commande N°', header_format)
+        sheet_1.write(0, 3, 'Client', header_format)
+        sheet_1.write(0, 4, 'Heures prévues initialement', header_format)
+        sheet_1.write(0, 5, f'Heures passés entre le {begin_date.strftime("%d/%m/%Y")} et {end_date.strftime("%d/%m/%Y")}', header_format)
+        sheet_1.write(0, 6, f'Heures restantes le {end_date.strftime("%d/%m/%Y")}', header_format)
+        sheet_1.write(0, 7, 'Société', header_format)
+        
+        line = 1
+        for data_line in data_not_consumed:
+            sheet_1.write(line, 0, data_line["create_date"])
+            sheet_1.write(line, 1, data_line["date_deadline"])
+            sheet_1.write(line, 2, data_line["command_number"])
+            sheet_1.write(line, 3, data_line["client"])
+            sheet_1.write(line, 4, data_line["planned_hours"])
+            sheet_1.write(line, 5, data_line["effective_hours"])
+            sheet_1.write(line, 6, data_line["remaining_hours"],green_text)
+            sheet_1.write(line, 7, data_line["company"])
+            line += 1
+        
+        #CH CONSOMMES
+        sheet_2 = workbook.add_worksheet("CH CONSOMMES")
+        sheet_2.set_column(0, 8, 30)
+        
+        sheet_2.write(0, 0, 'Date de création du contrat', header_format)
+        sheet_2.write(0, 1, "Date d'expiration", header_format)
+        sheet_2.write(0, 2, 'Bon de commande N°', header_format)
+        sheet_2.write(0, 3, 'Client', header_format)
+        sheet_2.write(0, 4, 'Heures prévues initialement', header_format)
+        sheet_2.write(0, 5, 'Heures passés entre le 01/01/2022 et 31/12/2022', header_format)
+        sheet_2.write(0, 6, 'Heures restantes le 31/12/2022', header_format)
+        sheet_2.write(0, 7, 'Archivé à ce jour ?', header_format)
+        sheet_2.write(0, 8, 'Société', header_format)
+        
+        line = 1
+        for data_line in data_consumed:
+            sheet_2.write(line, 0, data_line["create_date"])
+            sheet_2.write(line, 1, data_line["date_deadline"])
+            sheet_2.write(line, 2, data_line["command_number"])
+            sheet_2.write(line, 3, data_line["client"])
+            sheet_2.write(line, 4, data_line["planned_hours"])
+            sheet_2.write(line, 5, data_line["effective_hours"])
+            sheet_2.write(line, 6, data_line["remaining_hours"],red_text)
+            if data_line["active"] :
+                sheet_2.write(line, 7, "OUI",green_text)
+            else:
+                sheet_2.write(line, 7, "NON",red_text)
+            sheet_2.write(line, 8, data_line["company"])
+            line += 1
+        
         workbook.close()
         output.seek(0)
         response.stream.write(output.read())
         output.close()
         return response
+
+    #Fonction de génération des contrats d'heures non consommés
+    #Prend en entrée l'objet request d'odoo, la date de début du calcul et la date de fin
+    def get_hour_contract_data (request, begin_date, end_date):
+        data_not_consumed = []
+        data_consumed = []
+        tasks = request.env["project.task"].search(['&','|',('active','=',False),('active','=',True),'&',('project_id.name','ilike','HEURES'),('project_id.name','ilike','CONTRAT D')],order='create_date desc')
+        
+        for task in tasks :
+            #Calcul de la deadline
+            date_deadline = ""
+            if task.date_deadline :
+                date_deadline = task.date_deadline.strftime("%d/%m/%Y")
+            #On détermine le pays du CH
+            company = ""
+            project_name = task.project_id.name
+            if "GPE" in project_name :
+                company = "SINERGIS GPE"
+            elif "MQE" in project_name :
+                company = "SINERGIS MQE"
+            elif "GUY" in project_name :
+                company = "SINERGIS GUY"
+            elif "BRD" in project_name :
+                company = "SINERGIS BRD"
+            
+            # Calcul des heures restantes et heures réalisées
+            remaining_hours = task.planned_hours # Heures restantes
+            effective_hours = 0 # Heures réalisées
+            # On soustrait ensuite chaque intervention avant la date demandée
+            timesheet_ids = request.env["account.analytic.line"].search([('task_id', '=', task.id),('create_date','<=',end_date.strftime("%Y-%m-%d"))])
+            for timesheet_id in timesheet_ids :
+                remaining_hours -= timesheet_id.unit_amount
+                if timesheet_id.date >= begin_date :
+                    effective_hours += timesheet_id.unit_amount
+            remaining_hours = round(remaining_hours, 1)
+            
+            element = {
+                "create_date" : task.create_date.strftime("%d/%m/%Y %H:%M:%S"),
+                "date_deadline" : date_deadline, #COMPUTE
+                "command_number" : task.sale_order_id.name,
+                "client" : task.partner_id.name,
+                "planned_hours" : task.planned_hours,
+                "effective_hours" : effective_hours,#task.effective_hours,
+                "remaining_hours" : remaining_hours, #COMPUTE
+                "active" : task.active,
+                "company" : company, #COMPUTE
+                }
+            
+            #On regarde si à end_date, le contrat d'heure est consommé ou non
+            if remaining_hours <= 0 :
+                data_consumed.append(element)
+            else :
+                data_not_consumed.append(element)
+        return data_consumed, data_not_consumed
+    
