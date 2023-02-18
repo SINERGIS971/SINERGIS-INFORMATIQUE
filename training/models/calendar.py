33ee8c0948a849c4e52f31331d32a2a49d53134b
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-18 15:12:39 +0100

    MàJ

--- training/models/calendar.py
+++ training/models/calendar.py
@@ -6,6 +6,10 @@ class CalendarEvent(models.Model):
     is_training = fields.Boolean(string="Est une formation ?",default=False)
     training_id = fields.Many2one("training",string="Formation",default=False)
 
+    # Bouton de supréssion de l'évènement dans des heures planifiées par le consultant
+    def training_remove_event_button (self):
+        self.unlink()
+
     @api.onchange("is_training")
     def on_change_is_training (self):
         if self.is_training == False:
