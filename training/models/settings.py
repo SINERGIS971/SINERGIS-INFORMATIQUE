6a800d0fb8dc2111b4714d1509f53a8c4364baca
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-19 10:32:29 +0100

    MàJ

--- training/models/settings.py
+++ training/models/settings.py
@@ -5,18 +5,22 @@ class TrainingSettings(models.TransientModel):
 
     training_cgv = fields.Binary(string="CGV")
     training_booklet = fields.Binary(string="Livret de formation")
+    diagnostic_initial = fields.Binary(string="Diagnostic initial")
 
     @api.model
     def get_values(self):
         res = super(TrainingSettings, self).get_values()
         ICPSudo = self.env['ir.config_parameter'].sudo()
         training_booklets = ICPSudo.get_param('training.training_booklet')
+        diagnostic_initial = ICPSudo.get_param('training.diagnostic_initial')
         res.update(
             training_booklet = training_booklets,
+            diagnostic_initial = diagnostic_initial,
         )
         return res
 
     def set_values(self):
         res = super(TrainingSettings, self).set_values()
         self.env['ir.config_parameter'].set_param('training.training_booklet', self.training_booklet)
+        self.env['ir.config_parameter'].set_param('training.diagnostic_initial', self.diagnostic_initial)
         return res
