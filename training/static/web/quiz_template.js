282d368e72753d779ae07d0432062b68578fa809
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-20 09:33:06 +0100

    MàJ

--- training/static/web/quiz_template.js
+++ training/static/web/quiz_template.js
@@ -1,12 +1,12 @@
 function submit_form() {
-  const token = getElementByXpath("//input[@name='token'][1]").value;
+  const token = getElementByXPath("//input[@name='token'][1]").value;
   const questions = [];
   const answers = [];
   var elements = document.getElementsByTagName("label");
   for (let i = 0; i < elements.length; i++) {
     questions.push(elements[i].innerHTML);
     name = elements[i].getAttribute("for");
-    answers.push(getElementByXpath("//*[@name='"+name+"'][1]").value);
+    answers.push(getElementByXPath("//*[@name='"+name+"'][1]").value);
   }
   //POST Request
   var xhr = new XMLHttpRequest();
@@ -19,6 +19,40 @@ function submit_form() {
   }));
 }
 
-function getElementByXpath(path) {
+function getElementByXPath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
+
+function getElementsByXPath(xpath) {
+  const elements = [];
+  const xpathResult = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+  let element = xpathResult.iterateNext();
+  while (element) {
+    elements.push(element);
+    element = xpathResult.iterateNext();
+  }
+  return elements;
+}
+
+function radio_hide(question_id, choice_id) {
+    elements = getElementsByXPath("//div[@hidden_by='"+question_id.toString()+"' and contains(@hidden_by_choice,';"+choice_id.toString()+";')]");
+    if (elements.length > 0) {
+        elements.forEach(function(element) {
+          inputs = element.querySelectorAll('input');
+              console.log(inputs);
+              inputs.forEach(input => {
+                input.value = '';
+                input.checked = false;
+          });
+          element.style.display = "none";
+        });
+    } else {
+        elements = getElementsByXPath("//div[@hidden_by='"+question_id.toString()+"' and not(contains(@hidden_by_choice,';"+choice_id.toString()+";'))]");
+        if (elements.length > 0) {
+            elements.forEach(function(element) {
+                element.style.display = "block";
+            });
+        }
+    }
+}
+
