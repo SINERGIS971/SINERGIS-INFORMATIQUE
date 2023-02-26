2da8b8d00e2b93377c30a210508f05d39db34f71
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-02-26 22:15:35 +0100

    MàJ

--- training/static/src/js/training.js
+++ training/static/src/js/training.js
@@ -1,8 +1,21 @@
-function trainingShowHideOldParts (e)
+function trainingShowHideOldParts ()
 {
-    checked = e.checked;
-    const major_parts = document.querySelectorAll('.major_part');
+    const checkbox = document.getElementById('training_show_hide_checkbox');
+    checked = checkbox.checked;
+    const actual_part = document.evaluate("//div[@name='state']/button[@aria-checked='true']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.getAttribute("data-value");
+    const major_parts = document.querySelectorAll('.training_major_part');
     major_parts.forEach(function(major_part) {
-        console.log(major_part.id);
+        const part = document.evaluate("//div[@class='training_major_part' and @id='"+major_part.id+"']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        if (part != null){
+        if (major_part.id != actual_part) {
+            if (checked) {
+                part.style.display = "block";
+            } else {
+                part.style.display = "none";
+            }
+        } else {
+            part.style.display = "block";
+        }
+        }
     });
 }
