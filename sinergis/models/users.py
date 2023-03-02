0242f1a78b05673451e22258f55dbd8a72138b4d
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-03-02 10:41:02 +0100

    maj

--- sinergis/models/users.py
+++ sinergis/models/users.py
@@ -8,3 +8,9 @@ class ResUsers(models.Model):
     x_sinergis_res_users_initials = fields.Char(string="Initiales")
     x_sinergis_res_users_tampon_signature = fields.Image(string="Signature document")
     x_sinergis_res_users_job = fields.Selection([('CONSULTANT', 'CONSULTANT'), ('COMMERCIAL', 'COMMERCIAL')], string="Profil")
+    x_sinergis_res_users_is_employee = fields.Boolean(string="Est employé", compute="_compute_x_sinergis_res_users_is_employee")
+
+    @api.depends('x_sinergis_res_users_is_employee')
+    def _compute_x_sinergis_res_users_is_employee (self):
+        for rec in self:
+            rec.x_sinergis_res_users_is_employee = rec.has_group('base.group_user')
