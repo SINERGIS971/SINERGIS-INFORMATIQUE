bec7d7f8448e5fc1621969676190a7cccaa3227c
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-03-26 17:50:34 +0200

    Update all_companies.js

--- sinergis/static/src/js/all_companies.js
+++ sinergis/static/src/js/all_companies.js
@@ -1,6 +1,15 @@
 odoo.define('sinergis.all_companies', function(require) {
     "use strict";
 
-    console.log($.bbq.getState('cids'));
+    var utils = require('web.utils');
+
+    $(document).ready(function() {
+        if ($.bbq.getState('cids') != "1,2,3,5") {
+            utils.set_cookie('cids', "1,2,3,5");
+            $.bbq.pushState({'cids': "1,2,3,5"}, 0);
+            location.reload();
+        }
+    });
+        
 
 });
