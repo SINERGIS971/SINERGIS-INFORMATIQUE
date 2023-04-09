035eb048e847b1af1700e3c26c49879e7a07c026
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-04-09 18:28:26 +0200

    MàJ

--- sinergis/models/users.py
+++ sinergis/models/users.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
@@ -11,7 +13,14 @@ class ResUsers(models.Model):
     #2 Mars 2023 : Pour utiliser directement dans les "domain" la condition de "utilisateur est employé"
     x_sinergis_res_users_is_employee = fields.Boolean(string="Est employé", compute="_compute_x_sinergis_res_users_is_employee", store=True)
 
+    # 9 Avril 2023 : Date du dernier changement du mot de passe pour la fonctionnalité "Changement de mot de passe tous les 90 jours"
+    x_sinergis_res_users_password_last_update = fields.Datetime(string="Date du dernier changement de mot de passe")
+
     @api.depends('x_sinergis_res_users_is_employee')
     def _compute_x_sinergis_res_users_is_employee (self):
         for rec in self:
             rec.x_sinergis_res_users_is_employee = rec.has_group('base.group_user')
+
+    @api.onchange('password')
+    def onchange_password(self):
+        self.x_sinergis_res_users_password_last_update = datetime.now()
