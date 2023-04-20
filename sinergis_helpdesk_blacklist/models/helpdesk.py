fde39867ff8daf5adecf38c830d3dbe8e1ed6575
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-04-20 19:16:49 +0200

    MàJ

--- sinergis_helpdesk_blacklist/models/helpdesk.py
+++ sinergis_helpdesk_blacklist/models/helpdesk.py
@@ -1,9 +1,20 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
+class HelpdeskTicket(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    def create (self, values):
+        email = self.email
+        if email in values:
+            if self.env['helpdesk.ticket.blacklist'].search(('email', '=', email)):  # If email in blacklist
+                return
+        return super(HelpdeskTicket, self).write(values)
+
+
 
 class HelpdeskTicketBlacklist(models.Model):
     _name = "helpdesk.ticket.blacklist"
     _description = "Ticket blacklist"
-
-    email = fields.Char(string="email", required=True)
+    _rec_name = "email"
+    email = fields.Char(string="Email", required=True)
