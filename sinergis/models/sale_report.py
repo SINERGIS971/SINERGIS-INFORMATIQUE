62133e543b82dcb941b51e33dcceac69efb23ea2
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-04-29 17:12:47 +0200

    maj

--- sinergis/models/sale_report.py
+++ sinergis/models/sale_report.py
@@ -27,9 +27,16 @@ class SinergisSaleReport(models.Model):
         ('done', 'Sales Done'),
         ('cancel', 'Cancelled'),
         ], string='Status', readonly=True)
-    margin = fields.Float('Margin')
+    
+    margin = fields.Float('Marge', readonly=True)
     
     order_id = fields.Many2one('sale.order', 'Vente', readonly=True)
+    
+    task_id = fields.Many2one('project.task', 'Tâche', readonly=True)
+    
+    planned_hours = fields.Float("Heures prévues", readonly=True)
+    effective_hours = fields.Float("Heures réalisées", readonly=True)
+    remaining_hours = fields.Float("Heures restantes", readonly=True)
 
     def _select_sale(self, fields=None):
         if not fields:
@@ -39,16 +46,43 @@ class SinergisSaleReport(models.Model):
             l.product_id as product_id,
             t.uom_id as product_uom,
             CASE WHEN l.product_id IS NOT NULL THEN sum(l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as product_uom_qty,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_delivered / u.factor * u2.factor) ELSE 0 END as qty_delivered,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM((l.product_uom_qty - l.qty_delivered) / u.factor * u2.factor) ELSE 0 END as qty_to_deliver,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_invoiced / u.factor * u2.factor) ELSE 0 END as qty_invoiced,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.qty_to_invoice / u.factor * u2.factor) ELSE 0 END as qty_to_invoice,
             CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_total,
             CASE WHEN l.product_id IS NOT NULL THEN sum(l.price_subtotal / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as price_subtotal,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.untaxed_amount_to_invoice / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as untaxed_amount_to_invoice,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(l.untaxed_amount_invoiced / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) ELSE 0 END as untaxed_amount_invoiced,
+            count(*) as nbr,
             s.name as name,
             s.date_order as date,
             s.state as state,
             s.partner_id as partner_id,
             s.user_id as user_id,
             s.company_id as company_id,
+            s.campaign_id as campaign_id,
+            s.medium_id as medium_id,
+            s.source_id as source_id,
+            extract(epoch from avg(date_trunc('day',s.date_order)-date_trunc('day',s.create_date)))/(24*60*60)::decimal(16,2) as delay,
+            t.categ_id as categ_id,
+            s.pricelist_id as pricelist_id,
+            s.analytic_account_id as analytic_account_id,
+            s.team_id as team_id,
+            p.product_tmpl_id,
+            partner.country_id as country_id,
+            partner.industry_id as industry_id,
+            partner.commercial_partner_id as commercial_partner_id,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(p.weight * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as weight,
+            CASE WHEN l.product_id IS NOT NULL THEN sum(p.volume * l.product_uom_qty / u.factor * u2.factor) ELSE 0 END as volume,
+            l.discount as discount,
+            CASE WHEN l.product_id IS NOT NULL THEN sum((l.price_unit * l.product_uom_qty * l.discount / 100.0 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END))ELSE 0 END as discount_amount,
             s.id as order_id,
-            SUM(l.margin / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin
+            SUM(l.margin / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(pt.planned_hours) ELSE 0 END AS planned_hours,
+            CASE WHEN l.product_id IS NOT NULL THEN SUM(aal.total_unit_amount) ELSE 0 END AS effective_hours,
+            CASE WHEN SUM(aal.total_unit_amount) IS NOT NULL THEN (SUM(pt.planned_hours)-SUM(aal.total_unit_amount)) ELSE SUM(pt.planned_hours) END AS remaining_hours,
+            pt.id as task_id
         """
         for field in fields.values():
             select_ += field
@@ -64,6 +98,8 @@ class SinergisSaleReport(models.Model):
                     left join uom_uom u on (u.id=l.product_uom)
                     left join uom_uom u2 on (u2.id=t.uom_id)
                     left join product_pricelist pp on (s.pricelist_id = pp.id)
+                    left join (SELECT DISTINCT ON (sale_line_id) sale_line_id, id, planned_hours FROM project_task) pt on (l.id = pt.sale_line_id)
+                    left join (SELECT task_id, SUM(unit_amount) as total_unit_amount FROM account_analytic_line GROUP BY task_id) aal on (aal.task_id = pt.id)
                 %s
         """ % from_clause
         return from_
@@ -73,13 +109,27 @@ class SinergisSaleReport(models.Model):
             l.product_id,
             l.order_id,
             t.uom_id,
+            t.categ_id,
             s.name,
             s.date_order,
             s.partner_id,
             s.user_id,
             s.state,
             s.company_id,
-            s.id %s
+            s.campaign_id,
+            s.medium_id,
+            s.source_id,
+            s.pricelist_id,
+            s.analytic_account_id,
+            s.team_id,
+            p.product_tmpl_id,
+            partner.country_id,
+            partner.industry_id,
+            partner.commercial_partner_id,
+            l.discount,
+            s.id,
+            pt.id,
+            aal.task_id %s
         """ % (groupby)
         return groupby_
 
