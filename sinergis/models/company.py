cf54e78d71ca8e9297acb46a393933d709007e48
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-05-31 15:36:00 -0400

    MàJ

--- sinergis/models/company.py
+++ sinergis/models/company.py
@@ -6,4 +6,4 @@ class ResCompany(models.Model):
 
     x_sinergis_res_company_iban = fields.Char(string="IBAN")
     x_sinergis_res_company_bic = fields.Char(string="BIC")
-    
+    x_sinergis_allow_task_creation = fields.Boolean(string="Créer des tâches à la confirmation du devis", default=True)
