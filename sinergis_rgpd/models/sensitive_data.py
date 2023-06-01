3018d2cdfe19692ede65c11af27d4c0357861a5c
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-06-01 14:31:04 -0400

    maj

--- sinergis_rgpd/models/sensitive_data.py
+++ sinergis_rgpd/models/sensitive_data.py
@@ -8,14 +8,18 @@ class SinergisSensitiveData(models.Model):
     _description = "Données sensibles Sinergis"
     _inherit = ["mail.thread","mail.activity.mixin"]
 
+    is_create = fields.Boolean(default=False)
+
     partner_id = fields.Many2one("res.partner", string="Société", required=True)
-    user_id = fields.Many2one("res.users", string="Utilisateur",default=lambda self: self.env.user)
+    user_id = fields.Many2one("res.users", string="Utilisateur",default=lambda self: self.env.user, required=True)
     name = fields.Char(string='Reference', required=True)
     deletion_date = fields.Datetime(string="Date de suppression",default=lambda self: fields.Datetime.now() + timedelta(days=30), required=True)
 
     #Lors de la création de ticket via mail, ajouter automatiquement le contact et la société attribuée
     @api.model_create_multi
     def create(self, list_vals):
-        for vals in list_vals:
-            self.activity_schedule('sinergis_rgpd.activity_sensitive_data',summary='Supprimer une donnée sensible',note="Supprimer une donnée sensible",date_deadline=datetime.today()+timedelta(days=30),user_id=vals['user_id'])
-        return super(SinergisSensitiveData, self).create(list_vals)
+        objects = super(SinergisSensitiveData, self).create(list_vals)
+        for object in objects:
+            object.activity_schedule('sinergis_rgpd.activity_sensitive_data',summary='Supprimer une donnée sensible',note="Supprimer une donnée sensible",date_deadline=object.deletion_date,user_id=object.user_id.id)
+            object.is_create = True
+        return objects
