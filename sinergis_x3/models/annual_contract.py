3e5f6edbb8024d45e0c0e5ab560a2962a4a7b60d
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-06-12 11:54:52 -0400

    Update annual_contract.py

--- sinergis_x3/models/annual_contract.py
+++ sinergis_x3/models/annual_contract.py
@@ -1,10 +1,17 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+import requests
+import json
+import base64
+import sys
 
 class SinergisAnnualContracts(models.Model):
     _name = "sinergis_x3.annual_contract"
     _description = "Contrats Annuels"
 
     # Relié via code client X3
+    BPCORD = fields.Char(string="Code X3 du client")
     partner_id = fields.Many2one("res.partner",string="Client",required=True)
 
     # SORDER
@@ -40,7 +47,106 @@ class SinergisAnnualContracts(models.Model):
     PAYLOC = fields.Float(string="Montant payé comptabilisé")
     TMPLOC = fields.Float(string="Montant payé provisoire")
 
+    def load_xctrencours_x3 (self):
+        # Chargement authentification
+        user_x3 = self.env['ir.config_parameter'].sudo().get_param('sinergis_x3.user_x3')
+        password_x3 = self.env['ir.config_parameter'].sudo().get_param('sinergis_x3.password_x3')
+        authentication_token = base64.b64encode(f"{user_x3}:{password_x3}".encode('utf-8')).decode("ascii")
+        # Chargement URL
+        base_url = self.env['ir.config_parameter'].sudo().get_param('sinergis_x3.base_url_x3')
+        path_x3_contracts = self.env['ir.config_parameter'].sudo().get_param('sinergis_x3.path_x3_contracts')
+        # Parametres
+        url = base_url + path_x3_contracts
+        cookies = False # Initalisation des cookies
+        count_per_page = 1000 # Nombre d'items par page
+        next_exists = True # Permet de savoir si une page existe ensuite
+
+        headers = {'content-type': 'text/xml;charset=UTF-8',
+         'Authorization': f'Basic {authentication_token}'}
+        
+        while next_exists:
+            response = requests.get(f"{url}&count={str(count_per_page)}",
+                            headers=headers,
+                            cookies=cookies)
+            cookies = response.cookies
+            response_content = response.content
+            response_json = json.loads(response_content)
+            if not "$links" in response_json:
+                raise ValidationError("Impossible de se connecter à X3 pour charger les contrats annuels")
+            if not '$next' in response_json['$links']:
+                next_exists = False
+            else :
+                url = response_json['$links']['$next']["$url"]
+
+            #Chargement des données
+            for resource in response_json['$resources']:
+                SALFCY = resource["SALFCY"]
+                SOHTYP = resource["SOHTYP"]
+                CUSORDREF = resource["CUSORDREF"]
+                # Il manque la date de commande ORDDAT
+                BPCORD = resource["BPCORD"]
+                TSICOD0 = resource["TSICOD0"]
+                TSICOD1 = resource["TSICOD1"]
+                TSICOD2 = resource["TSICOD2"]
+                TSICOD4 = resource["TSICOD4"]
+
+                ITMDES = resource["ITMDES"]
+                X_SERNUM = resource["X_SERNUM"]
+                X_EVO = resource["X_EVO"]
+                X_COMEVO = resource["X_COMEVO"]
+                X_PERIOD = resource["X_PERIOD"]
+                X_RENOUVELE = resource["X_RENOUVELE"]
+                STRDAT = resource["STRDAT"]
+                ENDDAT = resource["ENDDAT"]
+                QTY = resource["QTY"]
+                SAU = resource["SAU"]
+                NETPRI = resource["NETPRI"]
+                PFM = resource["PFM"]
+                # Il manque X_RESILIE
+                # Il manque X_DATRESIL
+                LASINVNUM = resource["LASINVNUM"]
+                AMTLOC = resource["AMTLOC"]
+                PAYLOC = resource["PAYLOC"]
+                TMPLOC = resource["TMPLOC"]
+
+                # On regarde si l'élément existe déjà dans la base
+                entity = self.env['sinergis_x3.annual_contract'].search([('BPCORD','=',BPCORD),('CUSORDREF','=',CUSORDREF),('TSICOD0','=',TSICOD0),('TSICOD1','=',TSICOD1),('TSICOD2','=',TSICOD2),('TSICOD4','=',TSICOD4)])
+                if not entity :
+                    partner_id = self.env['res.partner'].search([("sinergis_x3_code","=",BPCORD)],limit=1)
+                    data = {
+                        "partner_id": partner_id,
+                        "SALFCY": SALFCY,
+                        "SOHTYP": SOHTYP,
+                        "CUSORDREF": CUSORDREF,
+                        "BPCORD": BPCORD,
+                        "TSICOD0": TSICOD0,
+                        "TSICOD1": TSICOD1,
+                        "TSICOD2": TSICOD2,
+                        "TSICOD4": TSICOD4,
+                        "ITMDES": ITMDES,
+                        "X_SERNUM": X_SERNUM,
+                        "X_EVO": X_EVO,
+                        "X_COMEVO": X_COMEVO,
+                        "X_PERIOD": X_PERIOD,
+                        "X_RENOUVELE": X_RENOUVELE,
+                        "STRDAT": STRDAT,
+                        "ENDDAT": ENDDAT,
+                        "QTY": QTY,
+                        "SAU": SAU,
+                        "NETPRI": NETPRI,
+                        "PFM": PFM,
+                        "LASINVNUM": LASINVNUM,
+                        "AMTLOC": AMTLOC,
+                        "PAYLOC": PAYLOC,
+                        "TMPLOC": TMPLOC
+                    }
+                    self.env['sinergis_x3.annual_contract'].create(data)
+            
+
+        
+
 class ResPartner(models.Model):
     _inherit = "res.partner"
+    _sql_constraints = [('x_sinergis_x3_annual_contract_unique', 'unique(x_sinergis_x3_annual_contract)', 'X3 code already exists!')]
 
     x_sinergis_x3_annual_contract = fields.One2many('sinergis_x3.annual_contract', 'partner_id', string="Contrats annuels X3", readonly=True)
