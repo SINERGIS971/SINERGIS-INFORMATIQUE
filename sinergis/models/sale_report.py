ae6eb94896ce60c380317e02ff801500cd1e6941
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-06-21 11:12:45 -0400

    Update sale_report.py

--- sinergis/models/sale_report.py
+++ sinergis/models/sale_report.py
@@ -88,17 +88,17 @@ class SinergisSaleReport(models.Model):
             CASE WHEN SUM(aal.total_unit_amount) IS NOT NULL THEN (SUM(pt.planned_hours)-SUM(aal.total_unit_amount)) ELSE SUM(pt.planned_hours) END AS remaining_hours,
             
             CASE
-                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 AND NOT s.x_sinergis_sale_order_solde_x3
+                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 = True AND s.x_sinergis_sale_order_solde_x3 = False
                 THEN sum(t.deposit_percentage*l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)
-                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 AND s.x_sinergis_sale_order_solde_x3
+                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 = True AND s.x_sinergis_sale_order_solde_x3 = True
                 THEN sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)
                 ELSE 0
             END as total_billed,
 
             CASE
-                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 AND NOT s.x_sinergis_sale_order_solde_x3
+                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 = True AND s.x_sinergis_sale_order_solde_x3 = False
                 THEN sum((1-t.deposit_percentage)*l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)
-                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 AND s.x_sinergis_sale_order_solde_x3
+                WHEN l.product_id IS NOT NULL AND s.x_sinergis_sale_order_acompte_x3 = True AND s.x_sinergis_sale_order_solde_x3 = True
                 THEN 0
                 ELSE sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)
             END as total_unbilled,
