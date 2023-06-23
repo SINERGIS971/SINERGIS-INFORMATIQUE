4bd2f096e2dda9baec8d3f32806bcc6c2510095c
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-06-23 08:38:10 -0400

    MàJ

--- sinergis/models/sentmail.py
+++ sinergis/models/sentmail.py
@@ -1,11 +1,1 @@
 from odoo import models, fields, api
-
-class SinergisMovementCountry(models.Model):
-    _name = "sinergis.sentmail"
-    _description = "Mails envoyés"
-    x_sinergis_sent_email_email = fields.One2many('mail.mail',compute="_compute_email",readonly=True)
-
-    @api.depends('x_sinergis_sent_email_email')
-    def _compute_x_sinergis_sent_email_email (self):
-        for event in self:
-            self.x_sinergis_helpdesk_ticket_taches = self.env["mail.mail"]
