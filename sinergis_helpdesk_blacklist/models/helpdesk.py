306415ed1429c3ce6b2a4e48a5b229e5b4b41e09
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-06-23 15:37:39 -0400

    Update helpdesk.py

--- sinergis_helpdesk_blacklist/models/helpdesk.py
+++ sinergis_helpdesk_blacklist/models/helpdesk.py
@@ -4,7 +4,7 @@ from odoo.exceptions import ValidationError
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    blacklisted = fields.Boolean(compute="_compute_blacklisted")
+    blacklisted = fields.Boolean(compute="_compute_blacklisted", store=True)
 
     @api.depends("blacklisted")
     def _compute_blacklisted(self):
@@ -25,3 +25,12 @@ class HelpdeskTicketBlacklist(models.Model):
     _description = "Ticket blacklist"
     _rec_name = "email"
     email = fields.Char(string="Email", required=True)
+
+    @api.model_create_multi
+    def create(self, list_value):
+        for vals in list_value:
+            tickets = self.env['helpdesk.ticket'].search([('x_sinergis_helpdesk_ticket_contact_mail','=',vals['email'])])
+            for ticket in tickets:
+                ticket._compute_blacklisted()
+        tickets = super(HelpdeskTicket, self).create(list_value)
+        return tickets
