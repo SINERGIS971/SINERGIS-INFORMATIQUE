8b7cb09621c38a0670d0e57ae037ce5179146b57
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-06-26 08:44:16 -0400

    maj

--- sinergis_helpdesk_blacklist/models/helpdesk.py
+++ sinergis_helpdesk_blacklist/models/helpdesk.py
@@ -8,10 +8,11 @@ class HelpdeskTicket(models.Model):
 
     @api.depends("blacklisted")
     def _compute_blacklisted(self):
-        if self.env["helpdesk.ticket.blacklist"].search([("email", "=", self.x_sinergis_helpdesk_ticket_contact_mail)]):
-            self.blacklisted = True
-        else:
-            self.blacklisted = False
+        for rec in self:
+            if self.env["helpdesk.ticket.blacklist"].search([("email", "=", rec.x_sinergis_helpdesk_ticket_contact_mail)]):
+                rec.blacklisted = True
+            else:
+                rec.blacklisted = False
 
     def action_register_blacklist(self):
         self.env["helpdesk.ticket.blacklist"].create({"email" : self.x_sinergis_helpdesk_ticket_contact_mail})
@@ -28,9 +29,17 @@ class HelpdeskTicketBlacklist(models.Model):
 
     @api.model_create_multi
     def create(self, list_value):
+        tickets = super(HelpdeskTicketBlacklist, self).create(list_value)
         for vals in list_value:
-            tickets = self.env['helpdesk.ticket'].search([('x_sinergis_helpdesk_ticket_contact_mail','=',vals['email'])])
-            for ticket in tickets:
-                ticket._compute_blacklisted()
-        tickets = super(HelpdeskTicket, self).create(list_value)
+            concerned_tickets = self.env['helpdesk.ticket'].search([('x_sinergis_helpdesk_ticket_contact_mail','=',vals['email'])])
+            for concerned_ticket in concerned_tickets:
+                concerned_ticket._compute_blacklisted()
         return tickets
+
+    def unlink(self):
+        email = self.email
+        super(HelpdeskTicketBlacklist, self).unlink()
+        concerned_tickets = self.env['helpdesk.ticket'].search([('x_sinergis_helpdesk_ticket_contact_mail','=',email)])
+        for concerned_ticket in concerned_tickets:
+            concerned_ticket._compute_blacklisted()
+        return
