cac7580df185b1e153821571cabc1030daac6f9e
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-03 12:42:14 -0400

    MàJ

--- sinergis/controllers/controllers.py
+++ sinergis/controllers/controllers.py
@@ -5,12 +5,14 @@ from odoo import http
 class SinergisCOntroller(http.Controller):
     @http.route('/sinergis/get_subproduct', auth='public', methods=['GET'])
     def index_get(self, **kw):
-        product_id = int(kw.get('product_id'))
-        subproduct_ids = http.request.env['sale.products.subproducts'].search([('product_id','=',product_id)])
-        data = []
-        for subproduct_id in subproduct_ids:
-            data.append({
-                'id': subproduct_id.id,
-                'name': subproduct_id.name
-            })
-        return str(data)
+        if "product_id" in kw:
+            product_id = int(kw.get('product_id'))
+            subproduct_ids = http.request.env['sale.products.subproducts'].search([('product_id','=',product_id)])
+            data = []
+            for subproduct_id in subproduct_ids:
+                data.append({
+                    'id': subproduct_id.id,
+                    'name': subproduct_id.name
+                })
+            return {'subproducts': data}
+        return ""
