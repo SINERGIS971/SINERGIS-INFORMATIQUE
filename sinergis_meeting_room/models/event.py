ae3a467bbf099f166e733dbb4643ab2714573274
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-05 09:45:40 -0400

    maj

--- sinergis_meeting_room/models/event.py
+++ sinergis_meeting_room/models/event.py
@@ -8,10 +8,13 @@ class SinergisMeetingRoomEvent(models.Model):
     _description = "Évènements en salle de réunion Sinergis"
 
     name = fields.Char(string='Reference', required=True)
-    room_id = fields.Many2one("sinergis_meeting_room.room",string="Salle")
+    room_id = fields.Many2one("sinergis_meeting_room.room",string="Salle", required=True)
     calendar_event_id = fields.Many2one("calendar.room",default=False,ondelete='cascade')
-    start_date = fields.Datetime(string='Début')
-    end_date = fields.Datetime(string='Fin')
+    start_date = fields.Datetime(string='Début', required=True)
+    end_date = fields.Datetime(string='Fin', required=True)
+
+    def download_meeting_room_sheet(self):
+        return self.env.ref('sinergis_meeting_room.meeting_room_sheet_report').report_action(self)
 
     @api.model_create_multi
     def create(self, list_value):
@@ -19,7 +22,7 @@ class SinergisMeetingRoomEvent(models.Model):
             start_date = vals['start_date']
             end_date = vals['end_date']
             room_id = vals["room_id"]
-            confront_events = self.env['sinergis_meeting_room.event'].search(['&','|','|',('room_id','=',room_id),'&',('start_date','<',start_date),('end_date','>',start_date),'&',('start_date','<',end_date),('end_date','>',end_date),'&',('start_date','>',start_date),('end_date','<',end_date)])
+            confront_events = self.env['sinergis_meeting_room.event'].search(['|','|','&',('room_id','=',room_id),'&',('start_date','<',start_date),('end_date','>',start_date),'&',('start_date','<',end_date),('end_date','>',end_date),'&',('start_date','>',start_date),('end_date','<',end_date)])
             if confront_events:
                 raise ValidationError('La salle de réunion est déjà réservée sur ce créneau.')
         events = super(SinergisMeetingRoomEvent, self).create(list_value)
