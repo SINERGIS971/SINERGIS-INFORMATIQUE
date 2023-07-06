0511f4dcd8445564ab2f3a49a1a878f37c8463be
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-06 11:18:09 -0400

    maj

--- sinergis_hotline_planning/models/event.py
+++ sinergis_hotline_planning/models/event.py
@@ -14,7 +14,12 @@ class SinergisHotlinePlanningEvent(models.Model):
     user_ids = fields.Many2many('res.users', string="Consultants")
 
     def print_calendar(self):
-        
+        data = {
+            "month": "Novembre",
+            "year": "2023",
+            "data": "<tr><td></td><td></td><td>1<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>2<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>3<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td></tr><tr><td>6<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>7<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>8<br></td><td>9<br></td><td>10<br></td></tr><tr><td>13<br></td><td>14<br></td><td>15<br></td><td>16<br></td><td>17<br></td></tr><tr><td>20<br></td><td>21<br></td><td>22<br></td><td>23<br></td><td>24<br></td></tr><tr><td>27<br></td><td>28<br></td><td>29<br></td><td>30<br></td></tr><tr></tr>",
+        }
+        return self.env.ref('sinergis_hotline_planning.sinergis_hotline_planning_event_sheet_report').report_action(self, data=data)
 
     @api.depends('display_name')
     def _compute_display_name (self):
