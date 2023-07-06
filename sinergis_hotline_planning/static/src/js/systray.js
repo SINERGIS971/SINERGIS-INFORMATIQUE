7d958e359c36f903695d5dfe53cb2bd9bd3cfa3e
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-06 15:57:03 -0400

    Update systray.js

--- sinergis_hotline_planning/static/src/js/systray.js
+++ sinergis_hotline_planning/static/src/js/systray.js
@@ -2,20 +2,20 @@
 import SystrayMenu from "web.SystrayMenu";
 import Widget from 'web.Widget';
 
-import { session } from "@web/session";
 
 var HotlinePlanningSystrayWidget = Widget.extend({
    template: 'HotlinePlanningSystray',
    events: {
-       'click #create_so': '_onClick',
+       'click #planning_hotline_so': '_onClick',
    },
    _onClick: function(){
+
     const date = new Date();
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
     let currentDate = `${year}-${month}-${day}`;
-    
+
        this.do_action({
             type: 'ir.actions.act_window',
             name: 'Hotline',
