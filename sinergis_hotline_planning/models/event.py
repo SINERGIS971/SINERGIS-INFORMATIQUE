1c9165ec951523f08e846d773c70e2ef40a71557
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-06 11:57:30 -0400

    maj

--- sinergis_hotline_planning/models/event.py
+++ sinergis_hotline_planning/models/event.py
@@ -1,7 +1,10 @@
 from odoo import api, models, fields, _, SUPERUSER_ID
 from odoo.exceptions import ValidationError
+from odoo.addons.sinergis_hotline_planning.utils.calendar_html import generate_calendar
 
 from datetime import datetime, timedelta
+
+import calendar
 
 class SinergisHotlinePlanningEvent(models.Model):
     _name = "sinergis_hotline_planning.event"
@@ -14,10 +17,26 @@ class SinergisHotlinePlanningEvent(models.Model):
     user_ids = fields.Many2many('res.users', string="Consultants")
 
     def print_calendar(self):
+        # Préparation des données
+        first_date = self.date.replace(day=1)
+        _, last_day = calendar.monthrange(self.date.year, self.date.month)
+        last_date = self.date.replace(day=last_day)
+        events = self.env['sinergis_hotline_planning.event'].search([('date','>',first_date),('date','<',last_date)])
+        planning = []
+        for event in events:
+            planning_day = {
+                "date": "2023-11-01",
+                "users": [],
+            }
+            for user_id in event.user_ids:
+                planning_day['users'].append(user_id.name)
+            planning.append(planning_day)
+
+
         data = {
             "month": "Novembre",
             "year": "2023",
-            "data": "<tr><td></td><td></td><td>1<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>2<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>3<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td></tr><tr><td>6<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>7<br>Esteban ANTONIO-MOTA, Magalie LEZEAU</td><td>8<br></td><td>9<br></td><td>10<br></td></tr><tr><td>13<br></td><td>14<br></td><td>15<br></td><td>16<br></td><td>17<br></td></tr><tr><td>20<br></td><td>21<br></td><td>22<br></td><td>23<br></td><td>24<br></td></tr><tr><td>27<br></td><td>28<br></td><td>29<br></td><td>30<br></td></tr><tr></tr>",
+            "data": generate_calendar(planning),
         }
         return self.env.ref('sinergis_hotline_planning.sinergis_hotline_planning_event_sheet_report').report_action(self, data=data)
 
