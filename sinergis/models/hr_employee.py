291517f4d51ac75e1abdd7cdd038031c6e726aac
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-11 15:23:08 -0400

    maj

--- sinergis/models/hr_employee.py
+++ sinergis/models/hr_employee.py
@@ -5,4 +5,6 @@ class HrEmployee(models.Model):
 
     cv_file = fields.Binary(string="CV")
     cv_filename = fields.Char(string="Nom du fichier CV")
+    cover_letter_file = fields.Binary(string="Lettre de motivation")
+    cover_letter_filename = fields.Char(string="Nom de la lettre de motivation")
     
