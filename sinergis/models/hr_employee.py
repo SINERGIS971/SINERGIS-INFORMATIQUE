27ffe16d6d3f280e490ef94fef12a6185eeb1d37
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-12 10:02:27 -0400

    Maj

--- sinergis/models/hr_employee.py
+++ sinergis/models/hr_employee.py
@@ -7,4 +7,16 @@ class HrEmployee(models.Model):
     cv_filename = fields.Char(string="Nom du fichier CV")
     cover_letter_file = fields.Binary(string="Lettre de motivation")
     cover_letter_filename = fields.Char(string="Nom de la lettre de motivation")
+
+    certification_ids = fields.One2many('hr.employee.certification', 'employee_id', string="Certifications")
+
+
+
+class HrEmployeeCertification(models.Model):
+    _name = "hr.employee.certification"
+    _description = "Certification"
     
+    employee_id = fields.Many2one('hr.employee')
+    name = fields.Char(string="Nom du Certificat", required=True)
+    file = fields.Binary(string="Fichier", required=True)
+    filename = fields.Char(string="Nom du fichier")
