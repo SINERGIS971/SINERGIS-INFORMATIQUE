c64a17ad3d4b413caf807ce2aef59dfdc0adf6da
Esteban ANTONIO-MOTA <99035516+SINERGIS971@users.noreply.github.com>
2023-07-17 12:30:31 -0400

    Update training.py

--- training/models/training.py
+++ training/models/training.py
@@ -234,7 +234,7 @@ class Training(models.Model):
     def _compute_duration_hours (self):
         for rec in self:
             #Une journée de formation correspond à 7 heures
-            rec.duration_hours = 7.0 * rec.duration
+            rec.duration_hours = int(7.0 * rec.duration)
 
     #Header buttons
 
