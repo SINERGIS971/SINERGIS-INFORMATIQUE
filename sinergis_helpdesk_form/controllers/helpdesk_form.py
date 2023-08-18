285cd5276b0fcd9fb2616813302306a2e881960e
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2023-08-18 10:25:56 +0200

    Update helpdesk_form.py

--- sinergis_helpdesk_form/controllers/helpdesk_form.py
+++ sinergis_helpdesk_form/controllers/helpdesk_form.py
@@ -77,10 +77,6 @@ class HelpdeskFormController(http.Controller):
                     data['partner_id'] = contact_id.id
                     data['x_sinergis_helpdesk_ticket_contact'] = contact_id.id
                 ticket = http.request.env['helpdesk.ticket'].sudo().create(data)
-                #if parent_id:
-                #    ticket.write({'x_sinergis_helpdesk_ticket_contact': contact_id.id})
-                #else:
-                #    ticket.write({'x_sinergis_helpdesk_ticket_contact': contact_id.id, 'partner_id': False})
 
                 # Création des PJ associées au ticket
                 for file in files :
