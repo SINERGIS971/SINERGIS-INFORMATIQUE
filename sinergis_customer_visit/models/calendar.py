59000be094e85d757d77b04915edb7117bbd75e9
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2023-08-26 22:27:01 +0200

    MàJ

--- sinergis_customer_visit/models/calendar.py
+++ sinergis_customer_visit/models/calendar.py
@@ -6,6 +6,7 @@ class CalendarEvent(models.Model):
     is_visit = fields.Boolean(string="Est une visite ?",default=False)
     visit_type = fields.Selection([('on_site', 'Sur site'),('phone', 'Par téléphone')], string="Type de visite")
     
+
     @api.onchange("is_visit")
     def on_change_is_visit (self):
         if self.is_training == False:
---
+++ sinergis_customer_visit/models/partner.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+    last_visit_date = fields.Datetime("Date de la dernière visite", compute="_compute_partner_visits")
+    last_visit_type = fields.Selection([('on_site', 'Sur site'),('phone', 'Par téléphone')], string="Type de visite", compute="_compute_partner_visits")
+    visit_state = fields.Selection([('no_visit', 'Pas de visite'),('missing_on_site', 'Manque une visite sur site'),('missing_on_site_or_phone', 'Manque une visite'), ('visited', 'Conditions de visite remplies')], string="Etat", compute="_compute_partner_visits")
+
+    @api.depends("last_visit_date", "visit_state")
+    def _compute_partner_visits (self):
+        for rec in self:
+            #Date de la dernière visite
+            last_visit = self.env["calendar.event"].search([('x_sinergis_calendar_event_client','=',rec.id),('is_visit','=',True)], order='start desc', limit=1)
+            if last_visit:
+                last_visit_date = last_visit.start
+                last_visit_type = last_visit.visit_type
+            else:
+                last_visit_date = False
+                last_visit_type = False
+
+            # Visites les 6 derniers mois
+            event_6_ids = self.env["calendar.event"].search([('x_sinergis_calendar_event_client','=',rec.id),
+                                                             ('is_visit','=',True),
+                                                             ('start','>=',(datetime.now()+relativedelta(months=-6)).strftime('%Y-%m-%d %H:%M:%S')),
+                                                             ('start','<=',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))], order='start desc')
+            # Visite sur site des 12 derniers mois
+            event_12_ids = self.env["calendar.event"].search([('x_sinergis_calendar_event_client','=',rec.id),
+                                                             ('is_visit','=',True),
+                                                             ('visit_type','=','on_site'),
+                                                             ('start','>=',(datetime.now()+relativedelta(months=-12)).strftime('%Y-%m-%d %H:%M:%S')),
+                                                             ('start','<=',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))], order='start desc')
+
+            if len(event_6_ids) > 0 and len(event_12_ids) > 0:
+                visit_state = "visited"
+            elif len(event_6_ids) == 0 and len(event_12_ids) > 0:
+                visit_state = "missing_on_site_or_phone"
+            elif len(event_6_ids) > 0 and len(event_12_ids) == 0:
+                visit_state = "missing_on_site"
+            else :
+                visit_state = "no_visit"
+            
+
+            
+            
+            
+
