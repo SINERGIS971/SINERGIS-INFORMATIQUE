d54ee65da90692dabbb733797f506d28be3c34e4
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2023-10-17 13:15:26 +0200

    maj

--- sinergis/models/reports_overview.py
+++ sinergis/models/reports_overview.py
@@ -2,7 +2,7 @@ from odoo import models, fields, api
 
 class MailMessage(models.Model):
     _inherit = "mail.message"
-    x_sinergis_partner_company_id = fields.Many2one("res.partner",string="Société du client",compute="_compute_x_sinergis_partner_company_id",store=True)
+    x_sinergis_partner_company_id = fields.Many2one("res.partner",string="Client",compute="_compute_x_sinergis_partner_company_id",store=True)
     x_sinergis_report_origin = fields.Char(string="Origine", compute="_compute_x_sinergis_report_origin",store=True)
 
     @api.depends("x_sinergis_partner_company_id")
@@ -15,10 +15,11 @@ class MailMessage(models.Model):
             else :
                 partner_company_id = False
                 for partner_id in rec.partner_ids:
-                    if partner_id.parent_id:
-                        partner_company_id = partner_id.parent_id
-                    else :
-                        partner_company_id = partner_id
+                    if partner_id != rec.author_id:
+                        if partner_id.parent_id:
+                            partner_company_id = partner_id.parent_id
+                        else :
+                            partner_company_id = partner_id
                 rec.x_sinergis_partner_company_id = partner_company_id
             
     @api.depends("x_sinergis_report_origin")
