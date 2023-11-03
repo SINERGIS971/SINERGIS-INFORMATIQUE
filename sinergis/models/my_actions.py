385d509f81750081df24675af46e61a7c6219af3
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2023-11-03 15:19:53 +0100

    Update my_actions.py

--- sinergis/models/my_actions.py
+++ sinergis/models/my_actions.py
@@ -115,7 +115,11 @@ class MyActions(models.Model):
                     
                     ht.user_id as consultant,
                     ru.company_id as company_id,
-                    rp.company_id as partner_company_id,
+                    CASE
+                        WHEN rp.company_id = FALSE
+                            THEN ru.company_id
+                            ELSE rp.company_id
+                        END AS partner_company_id,
                     ht.x_sinergis_helpdesk_ticket_contact as contact,
                     ht.x_sinergis_helpdesk_ticket_start_time as start_time,
                     ht.x_sinergis_helpdesk_ticket_end_time as end_time,
@@ -178,7 +182,11 @@ class MyActions(models.Model):
                     END AS time,
                     ce.user_id as consultant,
                     ru.company_id as company_id,
-                    rp.company_id as partner_company_id,
+                    CASE
+                        WHEN rp.company_id = FALSE
+                            THEN ru.company_id
+                            ELSE rp.company_id
+                        END AS partner_company_id,
                     ce.x_sinergis_calendar_event_contact as contact,
                     ce.x_sinergis_calendar_event_start_time as start_time,
                     ce.x_sinergis_calendar_event_end_time as end_time,
