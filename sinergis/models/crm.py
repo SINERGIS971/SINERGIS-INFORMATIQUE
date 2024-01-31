a0aae4ba6080acfb7a22649a0862191f9efadc02
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2024-01-31 09:24:13 +0700

    maj

--- sinergis/models/crm.py
+++ sinergis/models/crm.py
@@ -6,6 +6,8 @@ from datetime import datetime
 class CrmLead(models.Model):
     _inherit = "crm.lead"
     x_sinergis_crm_lead_contact = fields.Many2one("res.partner",string="Contact")
+    x_sinergis_crm_lead_contact_email = fields.Char(string="Email du contact", related="x_sinergis_crm_lead_contact.email")
+    x_sinergis_crm_lead_contact_phone = fields.Char(string="Tel. du contact", related="x_sinergis_crm_lead_contact.phone")
 
     @api.onchange("partner_id")
     def on_change_partner_id(self):
