87cf570cc51ec2bbb91c7ae587eb330e4189ac68
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2024-02-26 08:21:46 +1100

    maj

--- training/models/other_document.py
+++ training/models/other_document.py
@@ -11,11 +11,8 @@ class TrainingOtherDocument(models.Model):
     @api.onchange("type")
     def on_change_type(self):
         if self.env['training.other_document'].search_count([('type', '=', 'CGV')]) >= 1:
-                self.quiz_type = False
                 raise ValidationError("Le document 'CGV' existe déjà")
         if self.env['training.other_document'].search_count([('type', '=', 'Livret de formation')]) >= 1:
-                self.quiz_type = False
                 raise ValidationError("Le document 'Livret de formation' existe déjà")
         if self.env['training.other_document'].search_count([('type', '=', 'Diagnostic initial')]) >= 1:
-                self.quiz_type = False
                 raise ValidationError("Le document 'Diagnostic initial' existe déjà")
