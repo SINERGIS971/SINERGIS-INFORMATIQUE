4d090ff168200effd2d4c1f3422b5fde0234a478
SINERGIS971 <99035516+SINERGIS971@users.noreply.github.com>
2024-10-03 20:10:13 +0200

    maj

--- sinergis/static/src/js/exit_validation_helpdesk_ticket.js
+++ sinergis/static/src/js/exit_validation_helpdesk_ticket.js
@@ -6,25 +6,74 @@ odoo.define('sinergis.FormLeaveCheck', function (require) {
 
     FormController.include({
 
-        // Méthode appelée lorsque la vue va être changée
         on_detach_callback: function () {
             const record = this.model.get(this.handle);
 
-            // Vérifier si le modèle est 'helpdesk.ticket'
             if (record && record.model === 'helpdesk.ticket') {
-                const fieldValue = record.data.x_field_name; // Remplacer par le nom de votre champ
+                //const time = record.data.x_sinergis_helpdesk_ticket_temps_passe;
+                const deducted = record.data.x_sinergis_helpdesk_ticket_is_facturee;
+                const billing_type = record.data.x_sinergis_helpdesk_ticket_facturation;
+                const hours_contract = record.data.x_sinergis_helpdesk_ticket_tache2;
 
-                // Si le champ est vide, afficher la confirmation avant de quitter
-                if (!fieldValue) {
-                    const confirmation = confirm('Ce champ n\'est pas rempli. Êtes-vous sûr de vouloir quitter cette page ?');
-                    if (!confirmation) {
-                        // Bloquer la navigation
-                        return false;
-                    }
+                if (!deducted && (billing_type=="Contrat heures" || billing_type=="Devis")) {
+                    new Dialog(this, {
+                        title: 'Attention',
+                        size: 'medium',
+                        $content: $('<div>', {
+                            html: "Vous venez de quitter un ticket sans décompter d'heures sur la tâche ! "
+                        }),
+                        buttons: [{
+                            text: 'OK',
+                            close: true,
+                        }]
+                    }).open();
+                }
+                if (billing_type=="Contrat heures" && !hours_contract) {
+                    new Dialog(this, {
+                        title: 'Attention',
+                        size: 'medium',
+                        $content: $('<div>', {
+                            html: "Aucun contrat d'heures n'est rattaché à au ticket que vous venez de quitter !"
+                        }),
+                        buttons: [{
+                            text: 'OK',
+                            close: true,
+                        }]
+                    }).open();
+                }
+            } else if (record && record.model === 'calendar.event') {
+                //const time = record.data.x_sinergis_calendar_duree_facturee;
+                const deducted = record.data.x_sinergis_calendar_event_is_facturee;
+                const billing_type = record.data.x_sinergis_calendar_event_facturation;
+                const hours_contract = record.data.x_sinergis_calendar_event_tache2;
+
+                if (!deducted && (billing_type=="Contrat heure" || billing_type=="Devis")) {
+                    new Dialog(this, {
+                        title: 'Attention',
+                        size: 'medium',
+                        $content: $('<div>', {
+                            html: "Vous venez de quitter un évènement sans décompter d'heures sur la tâche ! "
+                        }),
+                        buttons: [{
+                            text: 'OK',
+                            close: true,
+                        }]
+                    }).open();
+                }
+                if (billing_type=="Contrat heure" && !hours_contract) {
+                    new Dialog(this, {
+                        title: 'Attention',
+                        size: 'medium',
+                        $content: $('<div>', {
+                            html: "Aucun contrat d'heures n'est rattaché à l'évènement que vous venez de quitter !"
+                        }),
+                        buttons: [{
+                            text: 'OK',
+                            close: true,
+                        }]
+                    }).open();
                 }
             }
-
-            // Appeler le comportement par défaut d'Odoo (permettre la navigation)
             return this._super.apply(this, arguments);
         },
     });
